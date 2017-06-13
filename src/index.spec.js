7cbfa51c07aecf3119c1acff09ed15513837fe4a
Matt Yao <mattyao1984@gmail.com>
2017-06-13 14:34:06 +1000

    Added simple unit test with Mocha and Chai

---
+++ src/index.spec.js
@@ -0,0 +1,8 @@
+var hearthstoneSkills = require('./index');
+var expect = require('chai').expect;
+
+describe('hearthstone-skills', function() {
+  it('should just work.', function() {
+    expect(true).to.be.true;
+  });
+})
