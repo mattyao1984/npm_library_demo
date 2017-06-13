68da3223e390278e878e2e9884339db00bbed86d
Matt Yao <mattyao1984@gmail.com>
2017-06-13 14:42:13 +1000

    Added meaningful tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,8 +1,21 @@
 var hearthstoneSkills = require('./index');
 var expect = require('chai').expect;
 
-describe('hearthstone-skills', function() {
-  it('should just work.', function() {
-    expect(true).to.be.true;
+describe('hearthstoneSkills - all', function() {
+  it('should return all data in an array of string', function() {
+    expect(hearthstoneSkills.all).to.satisfy(isArrayOfStrings);
+
+    function isArrayOfStrings(array) {
+      return array.every(function(item) {
+        return typeof item === 'string';
+      });
+    }
+  });
+});
+
+describe('hearthstoneSkills - random', function() {
+  it('should return a random string from the array', function() {
+    var randomItem = hearthstoneSkills.random();
+    expect(hearthstoneSkills.all).to.include(randomItem);
   });
 })
