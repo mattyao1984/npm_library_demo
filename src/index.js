0bb704bcd6536fb47e7170a6f385d56184547242
Matt Yao <mattyao1984@gmail.com>
2017-06-13 16:39:44 +1000

    feat(randomItems): Add new feature for multiple random items to be returned
    BREAKING CHANGE: closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 var hearthstoneSkills = require('./hearthstone-skills.json');
 var uniqueRandomArray = require('unique-random-array');
+var getRandomItem = uniqueRandomArray(hearthstoneSkills);
+
+var getRandomItems = function(number) {
+  if(number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomSkills = [];
+    for(var i = 0; i < number; i++) {
+      randomSkills.push(getRandomItem());
+    }
+
+    return randomSkills;
+  }
+}
 
 module.exports = {
   all: hearthstoneSkills,
-  random: uniqueRandomArray(hearthstoneSkills)
+  random: getRandomItems
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -18,4 +18,12 @@ describe('hearthstoneSkills - random', function() {
     var randomItem = hearthstoneSkills.random();
     expect(hearthstoneSkills.all).to.include(randomItem);
   });
+
+  it('should return an array of random items if passing a number', function() {
+    var randomItems = hearthstoneSkills.random(4);
+    expect(randomItems).to.have.length(4);
+    randomItems.every(function(item) {
+      expect(hearthstoneSkills.all).to.include(item);
+    });
+  });
 })
